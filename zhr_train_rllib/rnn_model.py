840fc2861051c431e1b667883f1f4dd94f315a1b
zhr <1021219803@qq.com>
2020-09-17 03:19:17 +0000

    add fc multi line

--- zhr_train_rllib/rnn_model.py
+++ zhr_train_rllib/rnn_model.py
@@ -8,6 +8,7 @@ from ray.rllib.models.torch.misc import AppendBiasLayer
 torch, nn = try_import_torch()
 
 from ray.rllib.models.torch.recurrent_net import RecurrentNetwork
+import numpy as np
 
 class RNNModel(RecurrentNetwork, nn.Module):
 
@@ -124,13 +125,14 @@ class RNNMultiModel(RecurrentNetwork, nn.Module):
         self.obs_size = _get_size(obs_space)
         self.rnn_hidden_dim = model_config["lstm_cell_size"]
         self.free_log_std = model_config.get("free_log_std")
-        num_outputs = num_outputs * 5
+
         if self.free_log_std:
             assert num_outputs % 2 == 0, (
                 "num_outputs must be divisible by two", num_outputs)
             num_outputs = num_outputs // 2
         if self.free_log_std:
             self.log_std = torch.nn.Parameter(torch.as_tensor([0.0] * num_outputs))
+        num_outputs = num_outputs * 5
 
         self.fc1 = nn.Linear(self.obs_size, self.rnn_hidden_dim)
         self.rnn = nn.GRU(self.rnn_hidden_dim, self.rnn_hidden_dim, batch_first=True)
@@ -146,10 +148,11 @@ class RNNMultiModel(RecurrentNetwork, nn.Module):
         You should implement forward_rnn() in your subclass."""
         if isinstance(seq_lens, np.ndarray):
             seq_lens = torch.Tensor(seq_lens).int()
+        condition = int(input_dict["obs"]["cline"]*5)
         output, new_state = self.forward_rnn(
             add_time_dimension(
                 input_dict["obs_flat"].float(), seq_lens, framework="torch"),
-            state, seq_lens)
+            state, seq_lens, condition)
         return torch.reshape(output, [-1, self.num_outputs]), new_state    
 
     @override(ModelV2)
@@ -163,10 +166,11 @@ class RNNMultiModel(RecurrentNetwork, nn.Module):
         return torch.reshape(self._cur_value, [-1])   
 
     @override(RecurrentNetwork)
-    def forward_rnn(self, input_dict, hidden_state, seq_lens):
+    def forward_rnn(self, input_dict, hidden_state, seq_lens, condition):
         x = nn.functional.relu(self.fc1(input_dict))
         x, h = self.rnn(x, torch.unsqueeze(hidden_state[0], 0))
         logits = self.fc2(x)
+        # logits_c = torch.gather
         if self.free_log_std:
             logstd = self.log_std.repeat(logits.shape[0], logits.shape[1], 1)
             logits = torch.cat([logits, logstd], dim=-1)
