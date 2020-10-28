eb4e5af80cf7116091402264be9ec51e56934dc2
zhr <1021219803@qq.com>
2020-10-28 12:24:31 +0000

    dataset 4lane_1

--- dataset/intersection_4lane/scenario.py
+++ dataset/intersection_4lane/scenario.py
@@ -9,9 +9,9 @@ from smarts.sstudio.types import (
 
 missions = [
     Mission(Route(begin=("edge-south-SN", 1, 40), end=("edge-west-EW", 0, 60))),
-    Mission(Route(begin=("edge-west-WE", 1, 50), end=("edge-east-WE", 0, 60))),
-    Mission(Route(begin=("edge-north-NS", 0, 40), end=("edge-south-NS", 1, 40))),
-    Mission(Route(begin=("edge-east-EW", 0, 50), end=("edge-west-EW", 1, 40))),
+    # Mission(Route(begin=("edge-west-WE", 1, 50), end=("edge-east-WE", 0, 60))),
+    # Mission(Route(begin=("edge-north-NS", 0, 40), end=("edge-south-NS", 1, 40))),
+    # Mission(Route(begin=("edge-east-EW", 0, 50), end=("edge-west-EW", 1, 40))),
 ]
 
 gen_scenario(
