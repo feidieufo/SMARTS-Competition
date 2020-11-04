d6f16e225d43fe65eb1e68fe4853143ec8e8e1be
zhr <1021219803@qq.com>
2020-11-04 11:12:05 +0000

    update benchmark

--- benchmark/agents/maddpg/maddpg.py
+++ benchmark/agents/maddpg/maddpg.py
@@ -1,12 +1,25 @@
+# Copyright (C) 2020. Huawei Technologies Co., Ltd. All rights reserved.
+#
+# Permission is hereby granted, free of charge, to any person obtaining a copy
+# of this software and associated documentation files (the "Software"), to deal
+# in the Software without restriction, including without limitation the rights
+# to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+# copies of the Software, and to permit persons to whom the Software is
+# furnished to do so, subject to the following conditions:
+#
+# The above copyright notice and this permission notice shall be included in
+# all copies or substantial portions of the Software.
+#
+# THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+# IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+# FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+# AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+# LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+# OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+# THE SOFTWARE.
 """Contributed port of MADDPG from OpenAI baselines.
 
-The implementation has a couple assumptions:
-- The number of agents is fixed and known upfront.
-- Each agent is bound to a policy of the same name.
-- Discrete actions are sent as logits (pre-softmax).
-
-For a minimal example, see rllib/examples/two_step_game.py,
-and the README for how to run with the multi-agent particle envs.
+Reference: https://github.com/ray-project/ray/blob/master/rllib/contrib/maddpg/maddpg.py
 """
 
 import logging
