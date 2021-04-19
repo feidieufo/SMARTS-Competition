7bf17cf6509a3cc4786a3ae03f2ecef2c7aa142a
zhr <1021219803@qq.com>
2021-04-19 15:07:39 +0000

    add ppo multiobj vtrace

---
+++ zhr_train_rllib/ppo_policy_modeldist_multiv_multiobj_vtrace.py
@@ -0,0 +1,472 @@
+import logging
+
+import ray
+from ray.rllib.agents.a3c.a3c_torch_policy import apply_grad_clipping
+from ray.rllib.agents.ppo.ppo_tf_policy import setup_config
+from ray.rllib.evaluation.postprocessing import Postprocessing
+from ray.rllib.policy.sample_batch import SampleBatch
+from ray.rllib.policy.torch_policy import EntropyCoeffSchedule, \
+    LearningRateSchedule
+from ray.rllib.policy.torch_policy_template import build_torch_policy
+from ray.rllib.utils.explained_variance import explained_variance
+from ray.rllib.utils.torch_ops import sequence_mask
+from ray.rllib.utils import try_import_torch
+from .fc_model import FCN_MultiV_MultiObj
+from ray.rllib.models.catalog import ModelCatalog
+from gym.spaces import Discrete
+import numpy as np
+import scipy
+
+torch, nn = try_import_torch()
+
+logger = logging.getLogger(__name__)
+
+from ray.rllib.models.torch.torch_action_dist import TorchDistributionWrapper
+from ray.rllib.models.action_dist import ActionDistribution
+from ray.rllib.utils.annotations import override
+
+decompose = 2
+class TorchCategorical(TorchDistributionWrapper):
+    """Wrapper class for PyTorch Categorical distribution."""
+
+    @override(ActionDistribution)
+    def __init__(self, inputs, model=None, temperature=1.0):
+        if temperature != 1.0:
+            assert temperature > 0.0, \
+                "Categorical `temperature` must be > 0.0!"
+            inputs /= temperature
+        inputs = inputs.view(inputs.shape[0], 9, decompose)
+        inputs = inputs.permute(2, 0, 1)
+        super().__init__(inputs, model)
+
+        self.dist = torch.distributions.categorical.Categorical(
+            logits=self.inputs)
+
+    @override(ActionDistribution)
+    def logp(self, actions):
+        actions = actions*torch.ones(size=self.inputs.shape[0:-1])
+        x =  self.dist.log_prob(actions)
+        return x.permute(1, 0)
+
+    @override(ActionDistribution)
+    def deterministic_sample(self):
+        self.last_sample = self.dist.probs.argmax(dim=1)
+        return self.last_sample
+
+    @staticmethod
+    @override(ActionDistribution)
+    def required_model_output_shape(action_space, model_config):
+        raise ValueError("No Need")
+        return action_space.n
+
+class TorchDiagGaussian(TorchDistributionWrapper):
+    """Wrapper class for PyTorch Normal distribution."""
+
+    @override(ActionDistribution)
+    def __init__(self, inputs, model):
+        super().__init__(inputs, model)
+        mean, log_std = torch.chunk(self.inputs, 2, dim=1)
+        self.dist = torch.distributions.normal.Normal(mean, torch.exp(log_std))
+
+    @override(ActionDistribution)
+    def deterministic_sample(self):
+        self.last_sample = self.dist.mean
+        return self.last_sample
+
+    @override(TorchDistributionWrapper)
+    def logp(self, actions):
+        return super().logp(actions).sum(-1)
+
+    @override(TorchDistributionWrapper)
+    def entropy(self):
+        return super().entropy().sum(-1)
+
+    @override(TorchDistributionWrapper)
+    def kl(self, other):
+        return super().kl(other).sum(-1)
+
+    @staticmethod
+    @override(ActionDistribution)
+    def required_model_output_shape(action_space, model_config):
+        return np.prod(action_space.shape) * 2
+
+
+class PPOLoss:
+    def __init__(self,
+                 dist_class,
+                 model,
+                 value_targets,
+                 advantages,
+                 actions,
+                 prev_logits,
+                 prev_actions_logp,
+                 vf_preds,
+                 curr_action_dist,
+                 value_fn,
+                 cur_kl_coeff,
+                 valid_mask,
+                 entropy_coeff=0,
+                 clip_param=0.1,
+                 vf_clip_param=0.1,
+                 vf_loss_coeff=1.0,
+                 use_gae=True):
+        """Constructs the loss for Proximal Policy Objective.
+        Arguments:
+            dist_class: action distribution class for logits.
+            value_targets (Placeholder): Placeholder for target values; used
+                for GAE.
+            actions (Placeholder): Placeholder for actions taken
+                from previous model evaluation.
+            advantages (Placeholder): Placeholder for calculated advantages
+                from previous model evaluation.
+            prev_logits (Placeholder): Placeholder for logits output from
+                previous model evaluation.
+            prev_actions_logp (Placeholder): Placeholder for prob output from
+                previous model evaluation.
+            vf_preds (Placeholder): Placeholder for value function output
+                from previous model evaluation.
+            curr_action_dist (ActionDistribution): ActionDistribution
+                of the current model.
+            value_fn (Tensor): Current value function output Tensor.
+            cur_kl_coeff (Variable): Variable holding the current PPO KL
+                coefficient.
+            valid_mask (Tensor): A bool mask of valid input elements (#2992).
+            entropy_coeff (float): Coefficient of the entropy regularizer.
+            clip_param (float): Clip parameter
+            vf_clip_param (float): Clip parameter for the value function
+            vf_loss_coeff (float): Coefficient of the value function loss
+            use_gae (bool): If true, use the Generalized Advantage Estimator.
+        """
+        if valid_mask is not None:
+            num_valid = torch.sum(valid_mask)
+
+            def reduce_mean_valid(t):
+                return torch.sum(t * valid_mask) / num_valid
+
+        else:
+
+            def reduce_mean_valid(t):
+                return torch.mean(t)
+
+        prev_dist = dist_class(prev_logits, model)
+        # Make loss functions.
+        logp_ratio = torch.exp(
+            curr_action_dist.logp(actions) - prev_actions_logp)
+        self.action_dist_prob = curr_action_dist.dist.probs.mean(dim=-2)
+        # logp_ratio = logp_ratio[:,0]
+        # logp_ratio = logp_ratio.mean(dim=-1)
+        action_kl = prev_dist.kl(curr_action_dist)
+        self.mean_kl = reduce_mean_valid(action_kl)
+
+        curr_entropy = curr_action_dist.entropy()
+        self.mean_entropy = reduce_mean_valid(curr_entropy)
+
+        # advantages = advantages.mean(-1)
+        # advantages = advantages[:,0]
+
+        surrogate_loss = torch.min(
+            advantages * logp_ratio,
+            advantages * torch.clamp(logp_ratio, 1 - clip_param,
+                                     1 + clip_param))
+        self.mean_policy_loss = reduce_mean_valid(-surrogate_loss)
+
+        if use_gae:
+            vf_loss1 = torch.pow(value_fn - value_targets, 2.0)
+            vf_clipped = vf_preds + torch.clamp(value_fn - vf_preds,
+                                                -vf_clip_param, vf_clip_param)
+            vf_loss2 = torch.pow(vf_clipped - value_targets, 2.0)
+            vf_loss = torch.max(vf_loss1, vf_loss2)
+            self.mean_vf_loss = reduce_mean_valid(vf_loss)
+            loss = reduce_mean_valid(
+                -surrogate_loss.mean(-1) + cur_kl_coeff * action_kl +
+                vf_loss_coeff * vf_loss.mean(-1) - entropy_coeff * curr_entropy)
+        else:
+            self.mean_vf_loss = 0.0
+            loss = reduce_mean_valid(-surrogate_loss.mean(-1) +
+                                     cur_kl_coeff * action_kl -
+                                     entropy_coeff * curr_entropy)
+        self.loss = loss
+
+def build_model_and_distribution(policy, obs_space, action_space, config):
+
+    if isinstance(action_space, Discrete):
+        num_outputs = action_space.n
+        dist = TorchCategorical
+    else:
+        num_outputs = np.prod(action_space.shape) * 2
+        dist = TorchDiagGaussian
+
+    model = ModelCatalog.get_model_v2(
+        obs_space=obs_space,
+        action_space=action_space,
+        num_outputs=num_outputs,
+        framework="torch",
+        model_interface=FCN_MultiV_MultiObj,
+        name="ac",
+        model_config=config["model"],
+        num_decompose=decompose)
+
+    return model, dist
+
+def ppo_surrogate_loss(policy, model, dist_class, train_batch):
+    logits, state = model.from_batch(train_batch)
+    action_dist = dist_class(logits, model)
+
+    mask = None
+    if state:
+        max_seq_len = torch.max(train_batch["seq_lens"])
+        mask = sequence_mask(train_batch["seq_lens"], max_seq_len)
+        mask = torch.reshape(mask, [-1])
+
+    policy.loss_obj = PPOLoss(
+        dist_class,
+        model,
+        train_batch[Postprocessing.VALUE_TARGETS],
+        train_batch[Postprocessing.ADVANTAGES],
+        train_batch[SampleBatch.ACTIONS],
+        train_batch[SampleBatch.ACTION_DIST_INPUTS],
+        # train_batch[SampleBatch.ACTION_LOGP],
+        train_batch["action_dist_logp"],
+        train_batch[SampleBatch.VF_PREDS],
+        action_dist,
+        model.value_function(),
+        policy.kl_coeff,
+        mask,
+        entropy_coeff=policy.entropy_coeff,
+        clip_param=policy.config["clip_param"],
+        vf_clip_param=policy.config["vf_clip_param"],
+        vf_loss_coeff=policy.config["vf_loss_coeff"],
+        use_gae=policy.config["use_gae"],
+    )
+
+    return policy.loss_obj.loss
+
+
+def kl_and_loss_stats(policy, train_batch):
+    return {
+        "cur_kl_coeff": policy.kl_coeff,
+        "cur_lr": policy.cur_lr,
+        "total_loss": policy.loss_obj.loss,
+        "policy_loss": policy.loss_obj.mean_policy_loss,
+        "vf_loss": policy.loss_obj.mean_vf_loss,
+        "vf_explained_var": explained_variance(
+            train_batch[Postprocessing.VALUE_TARGETS],
+            policy.model.value_function(),
+            framework="torch"),
+        "kl": policy.loss_obj.mean_kl,
+        "entropy": policy.loss_obj.mean_entropy,
+        "entropy_coeff": policy.entropy_coeff,
+    }
+
+
+def vf_preds_fetches(policy, input_dict, state_batches, model, action_dist):
+    """Adds value function outputs to experience train_batches."""
+    actions = input_dict[SampleBatch.ACTIONS]
+    assert actions.shape[0]==1
+    return {
+        SampleBatch.VF_PREDS: policy.model.value_function(),
+        "action_dist_logp": action_dist.logp(actions)
+    }
+
+def postprocess_ppo_gae(policy,
+                        sample_batch,
+                        other_agent_batches=None,
+                        episode=None):
+    """Adds the policy logits, VF preds, and advantages to the trajectory."""
+
+    completed = sample_batch["dones"][-1]
+    if completed:
+        last_r = torch.tensor([0.0]*decompose)
+    else:
+        next_state = []
+        for i in range(policy.num_state_tensors()):
+            next_state.append([sample_batch["state_out_{}".format(i)][-1]])
+        last_r = policy._value(sample_batch[SampleBatch.NEXT_OBS][-1],
+                               sample_batch[SampleBatch.ACTIONS][-1],
+                               sample_batch[SampleBatch.REWARDS][-1],
+                               *next_state)
+    batch = compute_advantages(
+        sample_batch,
+        last_r,
+        policy.config["gamma"],
+        policy.config["lambda"],
+        use_gae=policy.config["use_gae"])
+    return batch
+
+class KLCoeffMixin:
+    def __init__(self, config):
+        # KL Coefficient.
+        self.kl_coeff = config["kl_coeff"]
+        self.kl_target = config["kl_target"]
+
+    def update_kl(self, sampled_kl):
+        if sampled_kl > 2.0 * self.kl_target:
+            self.kl_coeff *= 1.5
+        elif sampled_kl < 0.5 * self.kl_target:
+            self.kl_coeff *= 0.5
+        return self.kl_coeff
+
+
+class ValueNetworkMixin:
+    def __init__(self, obs_space, action_space, config):
+        if config["use_gae"]:
+
+            def value(ob, prev_action, prev_reward, *state):
+                model_out, _ = self.model({
+                    SampleBatch.CUR_OBS: self._convert_to_tensor([ob]),
+                    SampleBatch.PREV_ACTIONS: self._convert_to_tensor(
+                        [prev_action]),
+                    SampleBatch.PREV_REWARDS: self._convert_to_tensor(
+                        [prev_reward]),
+                    "is_training": False,
+                }, [self._convert_to_tensor(s) for s in state],
+                                          self._convert_to_tensor([1]))
+                return self.model.value_function()[0]
+
+        else:
+
+            def value(ob, prev_action, prev_reward, *state):
+                return 0.0
+
+        self._value = value
+
+
+def setup_mixins(policy, obs_space, action_space, config):
+    ValueNetworkMixin.__init__(policy, obs_space, action_space, config)
+    KLCoeffMixin.__init__(policy, config)
+    EntropyCoeffSchedule.__init__(policy, config["entropy_coeff"],
+                                  config["entropy_coeff_schedule"])
+    LearningRateSchedule.__init__(policy, config["lr"], config["lr_schedule"])
+
+def apply_grad_clipping(policy, optimizer, loss):
+    info = {}
+    if policy.config["grad_clip"]:
+        for param_group in optimizer.param_groups:
+            # Make sure we only pass params with grad != None into torch
+            # clip_grad_norm_. Would fail otherwise.
+            params = list(
+                filter(lambda p: p.grad is not None, param_group["params"]))
+            if params:
+                grad_gnorm = nn.utils.clip_grad_norm_(
+                    params, policy.config["grad_clip"])
+                if isinstance(grad_gnorm, torch.Tensor):
+                    grad_gnorm = grad_gnorm.cpu().numpy()
+                info["grad_gnorm"] = grad_gnorm
+    for (i,probs) in enumerate(policy.loss_obj.action_dist_prob):
+        for (j, prob) in enumerate(probs):
+            info["policy_probs" + str(i) + "_" + str(j)] = prob.detach().numpy()
+    return info
+
+PPOTorchPolicy = build_torch_policy(
+    name="PPOTorchPolicy",
+    get_default_config=lambda: ray.rllib.agents.ppo.ppo.DEFAULT_CONFIG,
+    loss_fn=ppo_surrogate_loss,
+    stats_fn=kl_and_loss_stats,
+    make_model_and_action_dist=build_model_and_distribution,
+    # action_distribution_fn=
+    extra_action_out_fn=vf_preds_fetches,
+    postprocess_fn=postprocess_ppo_gae,
+    extra_grad_process_fn=apply_grad_clipping,
+    before_init=setup_config,
+    after_init=setup_mixins,
+    mixins=[
+        LearningRateSchedule, EntropyCoeffSchedule, KLCoeffMixin,
+        ValueNetworkMixin
+    ])
+
+
+def discount(x, gamma):
+    return scipy.signal.lfilter([1], [1, -gamma], x[::-1], axis=0)[::-1]
+
+
+class Postprocessing:
+    """Constant definitions for postprocessing."""
+
+    ADVANTAGES = "advantages"
+    VALUE_TARGETS = "value_targets"
+
+
+
+def compute_advantages(rollout,
+                       last_r,
+                       gamma=0.9,
+                       lambda_=1.0,
+                       use_gae=True,
+                       use_critic=True):
+    """
+    Given a rollout, compute its value targets and the advantage.
+
+    Args:
+        rollout (SampleBatch): SampleBatch of a single trajectory
+        last_r (float): Value estimation for last observation
+        gamma (float): Discount factor.
+        lambda_ (float): Parameter for GAE
+        use_gae (bool): Using Generalized Advantage Estimation
+        use_critic (bool): Whether to use critic (value estimates). Setting
+                           this to False will use 0 as baseline.
+
+    Returns:
+        SampleBatch (SampleBatch): Object with experience from rollout and
+            processed rewards.
+    """
+
+    traj = {}
+    trajsize = len(rollout[SampleBatch.ACTIONS])
+    for key in rollout:
+        traj[key] = np.stack(rollout[key])
+
+    assert SampleBatch.VF_PREDS in rollout or not use_critic, \
+        "use_critic=True but values not found"
+    assert use_critic or not use_gae, \
+        "Can't use gae without using a value function"
+
+    if use_gae:
+        nextnonterminal = (1.0 -  traj[SampleBatch.DONES])[:, np.newaxis]
+        vpred_t = np.concatenate(
+            [rollout[SampleBatch.VF_PREDS],
+             np.array(last_r.unsqueeze(0))])
+        delta_t = (
+            traj[SampleBatch.REWARDS] + gamma * vpred_t[1:] * nextnonterminal - vpred_t[:-1])
+
+        rho = np.exp(-traj[SampleBatch.ACTION_LOGP] + traj["action_dist_logp"])
+        rho = np.minimum(1.0, rho)
+
+        gaelam = np.zeros(shape=traj[SampleBatch.REWARDS].shape)
+        lastgaelam = 0 
+
+        for t in reversed(range(delta_t.shape[0])):
+            gaelam[t] = delta_t[t] + gamma * lambda_  * lastgaelam * nextnonterminal[t]
+            lastgaelam = rho[t] * gaelam[t]
+
+        traj[Postprocessing.ADVANTAGES] = gaelam
+        traj[Postprocessing.VALUE_TARGETS] = (rho*gaelam + vpred_t[:-1]).copy().astype(np.float32)
+
+        # This formula for the advantage comes from:
+        # "Generalized Advantage Estimation": https://arxiv.org/abs/1506.02438
+        # traj[Postprocessing.ADVANTAGES] = discount(delta_t, gamma * lambda_)
+        # traj[Postprocessing.VALUE_TARGETS] = (
+        #     traj[Postprocessing.ADVANTAGES] +
+        #     traj[SampleBatch.VF_PREDS]).copy().astype(np.float32)
+    else:
+        rewards_plus_v = np.concatenate(
+            [rollout[SampleBatch.REWARDS],
+             np.array([last_r])])
+        discounted_returns = discount(rewards_plus_v,
+                                      gamma)[:-1].copy().astype(np.float32)
+
+        if use_critic:
+            traj[Postprocessing.
+                 ADVANTAGES] = discounted_returns - rollout[SampleBatch.
+                                                            VF_PREDS]
+            traj[Postprocessing.VALUE_TARGETS] = discounted_returns
+        else:
+            traj[Postprocessing.ADVANTAGES] = discounted_returns
+            traj[Postprocessing.VALUE_TARGETS] = np.zeros_like(
+                traj[Postprocessing.ADVANTAGES])
+
+    traj[Postprocessing.ADVANTAGES] = traj[
+        Postprocessing.ADVANTAGES].copy().astype(np.float32)
+
+    assert all(val.shape[0] == trajsize for val in traj.values()), \
+        "Rollout stacked incorrectly!"
+    return SampleBatch(traj)
---
+++ zhr_train_rllib/stochastic_sampling_multiobj_vtrace.py
@@ -0,0 +1,110 @@
+from typing import Union
+
+from ray.rllib.models.action_dist import ActionDistribution
+from ray.rllib.models.modelv2 import ModelV2
+from ray.rllib.utils import try_import_tree
+from ray.rllib.utils.annotations import override
+from ray.rllib.utils.exploration.exploration import Exploration
+from ray.rllib.utils.from_config import from_config
+from ray.rllib.utils.schedules import Schedule, PiecewiseSchedule
+import random
+import math
+from ray.rllib.utils.framework import try_import_tf, try_import_torch, \
+    TensorType, get_variable
+
+tf = try_import_tf()
+torch, _ = try_import_torch()
+tree = try_import_tree()
+
+
+class StochasticSampling(Exploration):
+    """An exploration that simply samples from a distribution.
+
+    The sampling can be made deterministic by passing explore=False into
+    the call to `get_exploration_action`.
+    Also allows for scheduled parameters for the distributions, such as
+    lowering stddev, temperature, etc.. over time.
+    """
+
+    def __init__(self, action_space, *, framework: str, model: ModelV2,
+                 initial_epsilon=1.0,
+                 final_epsilon=0.05,
+                 epsilon_timesteps=int(1e5),
+                 epsilon_schedule=None,
+                 **kwargs):
+        """Initializes a StochasticSampling Exploration object.
+
+        Args:
+            action_space (Space): The gym action space used by the environment.
+            framework (str): One of None, "tf", "torch".
+        """
+        assert framework is not None
+        super().__init__(
+            action_space, model=model, framework=framework, **kwargs)
+
+        self.epsilon_schedule = \
+            from_config(Schedule, epsilon_schedule, framework=framework) or \
+            PiecewiseSchedule(
+                endpoints=[
+                    (0, initial_epsilon), (epsilon_timesteps, final_epsilon)],
+                outside_value=final_epsilon,
+                framework=self.framework)
+
+        self.last_timestep = get_variable(
+            0, framework=framework, tf_name="timestep")
+
+    @override(Exploration)
+    def get_exploration_action(self,
+                               *,
+                               action_distribution: ActionDistribution,
+                               timestep: Union[int, TensorType],
+                               explore: bool = True):
+        if self.framework == "torch":
+            return self._get_torch_exploration_action(action_distribution,
+                                                      explore, timestep)
+        else:
+            raise ValueError("Torch version does not support "
+                    "multiobj stochastic_sampling yet!")
+
+
+    def _get_torch_exploration_action(self, action_dist, explore, timestep):
+        self.last_timestep = timestep
+        inputs = action_dist.inputs
+        obj_num = inputs.shape[0]
+        assert inputs.shape[1]==1
+        action_p = torch.softmax(inputs, dim=-1)
+        epsilon = self.epsilon_schedule(self.last_timestep)
+        threshold = -0.5
+        a = torch.max(action_p[0], dim=-1)
+
+        y = (action_p[0] >=  torch.max(action_p[0], dim=-1)[0])
+        action_p_sup = [(action_p[i] >=  torch.max(action_p[i], dim=-1)[0] + threshold).int() for i in range(obj_num)]
+        valid = torch.ones_like(action_p_sup[0]).int()
+        for (i,action) in enumerate(action_p_sup):
+            last = valid
+            valid = valid & action
+
+            if not valid.bool().any():
+                valid = last
+                break
+        inputs_logits = inputs[i]
+        x = torch.where(valid!=0, inputs_logits, torch.tensor(0.0, dtype=torch.float32))
+        pos = x!=0.0
+        xx = x[pos]
+        dist = torch.distributions.categorical.Categorical(logits=xx)
+        label = torch.arange(0, 9).int().reshape(pos.shape)
+        label = label[pos]
+
+        if explore:
+            action = dist.sample()
+            logp = dist.log_prob(action)
+            logp = logp * torch.ones(size=(inputs.shape[1], inputs.shape[0]), dtype=torch.float32)
+            action = label[action]
+
+        else:
+            action = dist.deterministic_sample()
+            action = label[action]
+            logp = torch.zeros(size=(inputs.shape[1], inputs.shape[0]), dtype=torch.float32)
+
+        action = action.unsqueeze(0)
+        return action, logp
